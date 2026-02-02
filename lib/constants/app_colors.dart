353418c50d1ea30a953d2c65376dad75f9bf28ec
Den <dchaba@uwaterloo.ca>
2026-02-02 13:32:28 -0500

    Add color and text style constants

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const widgetBackground = Color.fromRGBO(42, 42, 62, 0.8);
+  static const cardBackground = Color.fromRGBO(58, 58, 78, 0.7);
+  static const accentBlue = Color(0xFF4A90D9);
+  static const textPrimary = Color(0xFFFFFFFF);
+  static const textSecondary = Color(0xFFB0B0B0);
+  static const inputBackground = Color.fromRGBO(26, 26, 46, 0.5);
+}
---
+++ lib/constants/app_text_styles.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const headerStyle = TextStyle(
+    color: Colors.white,
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const subHeaderStyle = TextStyle(
+    color: Color(0xFFB0B0B0),
+    fontSize: 12,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const taskTitleStyle = TextStyle(
+    color: Colors.white,
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const taskDescriptionStyle = TextStyle(
+    color: Color(0xFFB0B0B0),
+    fontSize: 11,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const inputHintStyle = TextStyle(
+    color: Color(0xFF808080),
+    fontSize: 13,
+  );
+}
