3d9ee5c228213ccfd544c265be335b87f244ddc1
Den <dchaba@uwaterloo.ca>
2026-02-02 13:49:00 -0500

    Add AddButton widget

---
+++ lib/widgets/add_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class AddButton extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  const AddButton({
+    super.key,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        width: 48,
+        height: 48,
+        decoration: BoxDecoration(
+          color: const Color.fromRGBO(58, 58, 78, 0.7),
+          shape: BoxShape.circle,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withValues(alpha: 0.2),
+              blurRadius: 8,
+              offset: const Offset(0, 2),
+            ),
+          ],
+        ),
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+          size: 28,
+        ),
+      ),
+    );
+  }
+}
