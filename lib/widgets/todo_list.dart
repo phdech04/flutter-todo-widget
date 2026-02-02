eed586963e8c427994058d4d26a8bde2bd7087f4
Den <dchaba@uwaterloo.ca>
2026-02-02 13:51:07 -0500

    Add TodoList widget

---
+++ lib/widgets/todo_list.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/todo_provider.dart';
+import 'todo_card.dart';
+
+class TodoList extends StatelessWidget {
+  const TodoList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TodoProvider>(
+      builder: (context, todoProvider, child) {
+        final todos = todoProvider.todos;
+
+        return ListView.builder(
+          padding: EdgeInsets.zero,
+          physics: const ClampingScrollPhysics(),
+          itemCount: todos.length,
+          itemBuilder: (context, index) {
+            final todo = todos[index];
+            return TodoCard(
+              todo: todo,
+              onDelete: () => todoProvider.removeTodo(todo.id),
+            );
+          },
+        );
+      },
+    );
+  }
+}
